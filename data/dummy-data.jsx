cf9286c7f6734d6c310358df6b187b56f9e2a5de
NANGSHEN HOM <nangshenhomrachel@NANGSHENs-Air.attlocal.net>
2021-06-08 17:27:52 -0400

    adding food categories

---
+++ data/dummy-data.jsx
@@ -0,0 +1,16 @@
+import Category from '../models/category';
+
+
+export const CATEGORIES = [
+    new Category('c1', 'Italian', '#f5428d'),
+    new Category('c2', 'Quick & Easy', '#f54242'),
+    new Category('c3', 'Hamburgers', '#f5a442'),
+    new Category('c4', 'German', '#f5d142'),
+    new Category('c5', 'Burmese', '#368dff'),
+    new Category('c6', 'Korean', '#41d95d'),
+    new Category('c7', 'Japanese', '#9eecff'),
+    new Category('c8', 'Thai', '#b9ffb0'),
+    new Category('c9', 'Keto', '#ffc7ff'),
+    new Category('c10', 'Glunten free ', '#47fced')
+
+];
